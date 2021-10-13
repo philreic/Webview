97ca8819724d24c75c80fe34967bc66b2d993c3b
philreic <91380439+philreic@users.noreply.github.com>
2021-10-13 17:32:50 +0200

    Initial Commit

---
+++ Webview/WebviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebviewApp.swift
+//  Webview
+//
+//  Created by Philippe Reichen on 10/13/21.
+//
+
+import SwiftUI
+
+@main
+struct WebviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
